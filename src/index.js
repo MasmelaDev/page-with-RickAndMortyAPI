4c1c33d9d92ea85e67d277bd43f7ec26578f1dee
MasmelaDev <miguelmasmeladev@gmail.com>
2023-02-17 13:53:28 -0500

    agregue contador de personajes, agregue un random para el numero de la pagina de la api, cambie el elemento parrafo del nombre de la card por un elemento a con el link hacia su objeto

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 const baseUrl = "https://rickandmortyapi.com/";
 const mainApp = document.getElementById("app");
+const random = Math.floor(Math.random() * 42);
+
+const charactersCount = document.querySelector(".text")
 
 window
-  .fetch(`${baseUrl}/api/character`)
-  .then((respuesta) => respuesta.json())
-  .then((responseJson) => {
+.fetch(`${baseUrl}/api/character?page=${random}`)
+.then((respuesta) => respuesta.json())
+.then((responseJson) => {
+    charactersCount.textContent = `${charactersCount.textContent}: ${responseJson.info.count}`
     const cards = [];
 
     responseJson.results.forEach((item) => {
       const image = document.createElement("img");
       image.src = item.image;
 
-      const name = document.createElement("p");
+      const name = document.createElement("a");
       name.textContent = item.name;
       name.className ="card__name"
+      name.href = item.url
+      name.setAttribute("target","__blank")
 
       const status = document.createElement("p")
       status.textContent = item.status
